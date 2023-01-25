134a1ca15715b188bc6c1e99d8eabab6ef40a01d
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2023-01-25 17:07:39 -0500

    fix indentation (only whitespace changes)

--- crates/rethnet_evm_napi/test/evm/StateManager.ts
+++ crates/rethnet_evm_napi/test/evm/StateManager.ts
@@ -24,82 +24,82 @@ describe("State Manager", () => {
 
   for (const {name, getStateManager} of stateManagers) {
     describe(`With the ${name} StateManager`, () => {
-  let stateManager: StateManager;
+      let stateManager: StateManager;
 
-  beforeEach(function () {
-    stateManager = getStateManager();
-  });
+      beforeEach(function () {
+        stateManager = getStateManager();
+      });
 
-  // TODO: insertBlock, setAccountCode, setAccountStorageSlot
-  it("getAccountByAddress", async () => {
-    await stateManager.insertAccount(caller.buf, {
-      nonce: 0n,
-      balance: 0n,
-      codeHash: KECCAK256_NULL,
-    });
-    let account = await stateManager.getAccountByAddress(caller.buf);
+      // TODO: insertBlock, setAccountCode, setAccountStorageSlot
+      it("getAccountByAddress", async () => {
+        await stateManager.insertAccount(caller.buf, {
+          nonce: 0n,
+          balance: 0n,
+          codeHash: KECCAK256_NULL,
+        });
+        let account = await stateManager.getAccountByAddress(caller.buf);
 
-    expect(account?.balance).to.equal(0n);
-    expect(account?.nonce).to.equal(0n);
-  });
+        expect(account?.balance).to.equal(0n);
+        expect(account?.nonce).to.equal(0n);
+      });
 
-  it("setAccountBalance", async () => {
-    await stateManager.insertAccount(caller.buf, {
-      nonce: 0n,
-      balance: 0n,
-      codeHash: KECCAK256_NULL,
-    });
+      it("setAccountBalance", async () => {
+        await stateManager.insertAccount(caller.buf, {
+          nonce: 0n,
+          balance: 0n,
+          codeHash: KECCAK256_NULL,
+        });
 
-    await stateManager.modifyAccount(
-      caller.buf,
-      async function (
-        _balance: bigint,
-        nonce: bigint,
-        code: Buffer | undefined
-      ): Promise<AccountData> {
-        return {
-          balance: 100n,
-          nonce,
-          code,
-        };
-      }
-    );
+        await stateManager.modifyAccount(
+          caller.buf,
+          async function (
+            _balance: bigint,
+            nonce: bigint,
+            code: Buffer | undefined
+          ): Promise<AccountData> {
+            return {
+              balance: 100n,
+              nonce,
+              code,
+            };
+          }
+        );
 
-    let account = await stateManager.getAccountByAddress(caller.buf);
+        let account = await stateManager.getAccountByAddress(caller.buf);
 
-    expect(account?.balance).to.equal(100n);
-    expect(account?.nonce).to.equal(0n);
-    expect(account?.codeHash).to.eql(KECCAK256_NULL);
-  });
+        expect(account?.balance).to.equal(100n);
+        expect(account?.nonce).to.equal(0n);
+        expect(account?.codeHash).to.eql(KECCAK256_NULL);
+      });
 
-  it("setAccountNonce", async () => {
-    await stateManager.insertAccount(caller.buf, {
-      nonce: 0n,
-      balance: 0n,
-      codeHash: KECCAK256_NULL,
-    });
+      it("setAccountNonce", async () => {
+        await stateManager.insertAccount(caller.buf, {
+          nonce: 0n,
+          balance: 0n,
+          codeHash: KECCAK256_NULL,
+        });
 
-    await stateManager.modifyAccount(
-      caller.buf,
-      async function (
-        balance: bigint,
-        nonce: bigint,
-        code: Buffer | undefined
-      ): Promise<AccountData> {
-        return {
-          balance,
-          nonce: 5n,
-          code,
-        };
-      }
-    );
+        await stateManager.modifyAccount(
+          caller.buf,
+          async function (
+            balance: bigint,
+            nonce: bigint,
+            code: Buffer | undefined
+          ): Promise<AccountData> {
+            return {
+              balance,
+              nonce: 5n,
+              code,
+            };
+          }
+        );
 
-    let account = await stateManager.getAccountByAddress(caller.buf);
+        let account = await stateManager.getAccountByAddress(caller.buf);
 
-    expect(account?.balance).to.equal(0n);
-    expect(account?.nonce).to.equal(5n);
-    expect(account?.codeHash).to.eql(KECCAK256_NULL);
-  });
+        expect(account?.balance).to.equal(0n);
+        expect(account?.nonce).to.equal(5n);
+        expect(account?.codeHash).to.eql(KECCAK256_NULL);
+      });
     });
   }
 });
